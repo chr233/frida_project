9bef866800ceffb8624ff5834637a41f3308d608
Chr_ <chr@chrxw.com>
2020-08-08 12:42:26 +0800

    update

--- hook.js
+++ hook.js
@@ -2,47 +2,49 @@
  * @Author       : Chr_
  * @Date         : 2020-02-16 18:42:39
  * @LastEditors  : Chr_
- * @LastEditTime : 2020-08-06 11:35:14
+ * @LastEditTime : 2020-08-08 12:34:33
  * @Description  : com.max.xiaoheihe
  */
 
 
 console.log("脚本载入成功");
 Java.perform(function () {
-    var charToJstringAddr = Module.findExportByName("libnative-lib.so", "charToJstring");
-    console.log(charToJstringAddr);
-    if (charToJstringAddr != null) {
-        Interceptor.attach(charToJstringAddr, {
-            onEnter: function (args) {
-                var arg0 = args[0];
-                var arg1 = args[1];
-                console.log('charToJstring - Enter');
-                console.log(arg0, Memory.readCString(arg0));
-                console.log(arg1, Memory.readCString(arg1));
-            },
-            onLeave: function (retval) {
-                //retval函数返回值
-                console.log('charToJstring - Leave');
-                console.log(retval.toString());
-                console.log('======');
-            }
-        });
-    }
-    // var ByteString = Java.use("com.android.okhttp.okio.ByteString");
-    // var gzip = Java.use('com.max.xiaoheihe.utils.ca');
-    // gzip.a.overload("java.lang.String").implementation = function (p1) {
-    //     console.log('gzip-in:', p1);
-    //     var ps = this.a(p1);
-    //     console.log('gzip-out:', ByteString.of(ps).hex());
-    //     return ps;
-    // };
-    // var uw = Java.use('com.max.xiaoheihe.utils.W');
-    // uw.d.overload("int").implementation = function (p1) {
-    //     console.log('genkey-in:', p1);
-    //     var ps = this.d(p1);
-    //     console.log('genkey-out:', ps);
-    //     return (ps);
+    // var charToJstringAddr = Module.findExportByName("libnative-lib.so", "charToJstring");
+    // console.log(charToJstringAddr);
+    // if (charToJstringAddr != null) {
+    //     Interceptor.attach(charToJstringAddr, {
+    //         onEnter: function (args) {
+    //             var arg0 = args[0];
+    //             var arg1 = args[1];
+    //             console.log('charToJstring - Enter');
+    //             console.log(arg0, Memory.readCString(arg0));
+    //             console.log(arg1, Memory.readCString(arg1));
+    //         },
+    //         onLeave: function (retval) {
+    //             //retval函数返回值
+    //             console.log('charToJstring - Leave');
+    //             console.log(retval.toString());
+    //             console.log('======');
+    //         }
+    //     });
     // }
+    var base64 = Java.use("android.util.Base64");
+    var gzip = Java.use('com.max.xiaoheihe.utils.ca');
+    gzip.a.overload("java.lang.String").implementation = function (p1) {
+        console.log('gzip-in:')
+        console.log( p1);
+        var ps = this.a(p1);
+        // console.log('gzip-out:', base64.encodeToString(ps,0));
+        return ps;
+    };
+    var uw = Java.use('com.max.xiaoheihe.utils.W');
+    uw.d.overload("int").implementation = function (p1) {
+        console.log('genkey-in:', p1);
+        var ps = this.d(p1);
+        ps='a12ds12b'
+        console.log('genkey-out:', ps);
+        return (ps);
+    }
     // var uw = Java.use('com.max.xiaoheihe.utils.W');
     // uw.d.overload("int").implementation = function (p1) {
     //     console.log('genkey-in:', p1);
