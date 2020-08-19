a7cc96aed69b4a0970ff2b705a16fd82f3bfbfdb
Chr_ <chr@chrxw.com>
2020-08-19 18:40:49 +0800

    update

--- hook.js
+++ hook.js
@@ -2,7 +2,7 @@
  * @Author       : Chr_
  * @Date         : 2020-02-16 18:42:39
  * @LastEditors  : Chr_
- * @LastEditTime : 2020-08-19 18:32:22
+ * @LastEditTime : 2020-08-19 18:40:44
  * @Description  : com.max.xiaoheihe
  */
 
@@ -109,23 +109,23 @@ Java.perform(function () {
     //     });
     // }
 
-    var base64 = Java.use("android.util.Base64");
-    var gzip = Java.use('com.max.xiaoheihe.utils.ca');
-    gzip.a.overload("java.lang.String").implementation = function (p1) {
-        console.log('gzip-in:')
-        console.log(p1);
-        var ps = this.a(p1);
-        // console.log('gzip-out:', base64.encodeToString(ps,0));
-        return ps;
-    };
-    var uw = Java.use('com.max.xiaoheihe.utils.W');
-    uw.d.overload("int").implementation = function (p1) {
-        console.log('genkey-in:', p1);
-        var ps = this.d(p1);
-        // ps='a12ds12b'
-        console.log('genkey-out:', ps);
-        return (ps);
-    }
+    // var base64 = Java.use("android.util.Base64");
+    // var gzip = Java.use('com.max.xiaoheihe.utils.ca');
+    // gzip.a.overload("java.lang.String").implementation = function (p1) {
+    //     console.log('gzip-in:')
+    //     console.log(p1);
+    //     var ps = this.a(p1);
+    //     // console.log('gzip-out:', base64.encodeToString(ps,0));
+    //     return ps;
+    // };
+    // var uw = Java.use('com.max.xiaoheihe.utils.W');
+    // uw.d.overload("int").implementation = function (p1) {
+    //     console.log('genkey-in:', p1);
+    //     var ps = this.d(p1);
+    //     // ps='a12ds12b'
+    //     console.log('genkey-out:', ps);
+    //     return (ps);
+    // }
     uw.b.overload("java.lang.String").implementation = function (p1) {
         console.log('md5-in:', p1);
         var ps = this.b(p1);
@@ -160,35 +160,3 @@ Java.perform(function () {
     //     });
     // }
 });
-
-
-// console.log("脚本载入成功");
-// Java.perform(function () {
-//     var JavaString = Java.use("java.lang.String");
-
-
-    // var encodeAddr = Module.findExportByName("libnative-lib.so", "getrsakey");
-    // console.log(encodeAddr);
-    // if (encodeAddr != null) {
-    //     Interceptor.attach(encodeAddr, {
-    //         onEnter: function (args) {
-    //             //args参数数组
-    //             console.log('Enter')
-    //             console.log(args[0], Memory.readCString(args[0]));
-    //             console.log(args[1], Memory.readCString(args[1]));
-    //             console.log(args[2], Memory.readCString(args[2]));
-    //             console.log(args[3], Memory.readCString(args[3]));
-    //             console.log(args[4], Memory.readCString(args[4]));
-    //         },
-    //         onLeave: function (retval) {
-    //             //retval函数返回值
-    //             console.log('Leave');
-    //             console.log(retval.toString());
-    //             console.log(Memory.readCString(retval));
-    //             console.log('======');
-    //             // retval.replace(0)  
-    //         }
-    //     });
-    // }
-// });
-
